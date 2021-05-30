d435509138b0dedd2fc698a6c8bc9392216259f9
Yousef Rabi <yousofrabi@gmail.com>
2021-05-30 12:52:00 +1000

    Add train_yolov5.sh script

--- src/train_yolov5.sh
+++ src/train_yolov5.sh
@@ -1,7 +1,9 @@
+set -eux pipefail
+
 CUDA_VISIBLE_DEVICES='0'
 
-python3 yolov5/train.py --img 640 --batch 128 --epochs 80 --data /home/yousef/deep-learning/kaggle/covid/data/processed/yolov5_data/train/images/fold_0.xml --device 0 --project trained-models/03-29_yolov5_fold0/ --weights 'yolov5x.pt'
-python3 yolov5/train.py --img 640 --batch 128 --epochs 80 --data /home/yousef/deep-learning/kaggle/covid/data/processed/yolov5_data/train/images/fold_1.xml --device 0 --project trained-models/03-29_yolov5_fold1/ --weights 'yolov5x.pt'
-python3 yolov5/train.py --img 640 --batch 128 --epochs 80 --data /home/yousef/deep-learning/kaggle/covid/data/processed/yolov5_data/train/images/fold_2.xml --device 0 --project trained-models/03-29_yolov5_fold2/ --weights 'yolov5x.pt'
-python3 yolov5/train.py --img 640 --batch 128 --epochs 80 --data /home/yousef/deep-learning/kaggle/covid/data/processed/yolov5_data/train/images/fold_3.xml --device 0 --project trained-models/03-29_yolov5_fold3/ --weights 'yolov5x.pt'
-python3 yolov5/train.py --img 640 --batch 128 --epochs 80 --data /home/yousef/deep-learning/kaggle/covid/data/processed/yolov5_data/train/images/fold_4.xml --device 0 --project trained-models/03-29_yolov5_fold4/ --weights 'yolov5x.pt'
+python3 yolov5/train.py --img 640 --batch 8 --epochs 80 --data yolov5/dataset_fold_0.yml --device 0 --project trained-models/03-29_yolov5_fold0/ --weights 'yolov5x.pt'
+python3 yolov5/train.py --img 640 --batch 8 --epochs 80 --data yolov5/dataset_fold_1.yml --device 0 --project trained-models/03-29_yolov5_fold1/ --weights 'yolov5x.pt'
+python3 yolov5/train.py --img 640 --batch 8 --epochs 80 --data yolov5/dataset_fold_2.yml --device 0 --project trained-models/03-29_yolov5_fold2/ --weights 'yolov5x.pt'
+python3 yolov5/train.py --img 640 --batch 8 --epochs 80 --data yolov5/dataset_fold_3.yml --device 0 --project trained-models/03-29_yolov5_fold3/ --weights 'yolov5x.pt'
+python3 yolov5/train.py --img 640 --batch 8 --epochs 80 --data yolov5/dataset_fold_4.yml --device 0 --project trained-models/03-29_yolov5_fold4/ --weights 'yolov5x.pt'
