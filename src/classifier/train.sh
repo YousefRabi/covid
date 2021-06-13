4129821c3f12c625e2f7f6447ad4db86b1ac3511
Yousef Rabi <yousofrabi@gmail.com>
2021-06-13 13:26:52 +1000

    train.sh and train_yolov5.sh

--- src/classifier/train.sh
+++ src/classifier/train.sh
@@ -1,6 +1,7 @@
 set -eux pipefail
 
-PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/12/fold-1.yml
-PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/12/fold-2.yml
-PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/12/fold-3.yml
-PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/12/fold-4.yml
+PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/18/fold-0.yml
+PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/18/fold-1.yml
+PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/18/fold-2.yml
+PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/18/fold-3.yml
+PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src' python src/classifier/train.py --config src/classifier/config/18/fold-4.yml
