5d552d48baaab6ede86b253d5a4baa2e9993ae8a
Yousef Rabi <yousofrabi@gmail.com>
2021-07-10 10:01:38 +1000

    Add train.sh for exp-83

--- src/classifier/train.sh
+++ src/classifier/train.sh
@@ -1,10 +1,10 @@
 set -eux pipefail
 
-export CUDA_VISIBLE_DEVICES=0
-export PYTHONPATH='/home/yousef/deep-learning/kaggle/covid-exp-81/src/'
+export CUDA_VISIBLE_DEVICES=1
+export PYTHONPATH='/home/yousef/deep-learning/kaggle/covid-exp-83/src/'
 
-python src/classifier/train.py --config src/classifier/config/81/fold-0.yml
-python src/classifier/train.py --config src/classifier/config/81/fold-1.yml
-python src/classifier/train.py --config src/classifier/config/81/fold-2.yml
-python src/classifier/train.py --config src/classifier/config/81/fold-3.yml
-python src/classifier/train.py --config src/classifier/config/81/fold-4.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-0.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-1.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-2.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-3.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-4.yml
