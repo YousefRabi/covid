7e0c4994204a687be80d8a78ae6ba37a21a2f1a1
Yousef Rabi <yousofrabi@gmail.com>
2021-08-14 08:02:11 +1000

    Add average precision to pytorch lightning runner

--- src/classifier/runner/pl_runner.py
+++ src/classifier/runner/pl_runner.py
@@ -1,32 +1,13 @@
-import os
-from pathlib import Path
-from easydict import EasyDict
-from collections import defaultdict
-
-import numpy as np
-
-import skimage.io
-import cv2
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 import torch
-from torch.cuda.amp import autocast
-from torch.utils.tensorboard import SummaryWriter
 from pytorch_lightning.core.lightning import LightningModule
+from torchmetrics import AveragePrecision
 
-from map_boxes import mean_average_precision_for_boxes
 
 from classifier.models import get_model
 from classifier.optimizers import get_optimizer
 from classifier.datasets import get_dataloader
 from classifier.transforms import get_transforms, get_first_place_melanoma_transforms, Mixup
 from classifier.losses import LossBuilder
-from classifier.schedulers import SchedulerBuilder
-from classifier.utils.utils import (fix_seed, enumerate_with_estimate,
-                                    save_model_with_optimizer, confusion_matrix_to_image)
-from classifier.utils.logconf import logging, formatter
 
 
 class LitModule(LightningModule):
@@ -43,56 +24,60 @@ class LitModule(LightningModule):
         self.trn_transforms = get_transforms(config.transforms.train)
         self.val_transforms = get_transforms(config.transforms.test)
 
-    def forward(self, x, return_mask):
+        self.average_precision = AveragePrecision(num_classes=4)
+
+    def forward(self, x, return_mask=False):
         return self.model(x, return_mask)
 
     def training_step(self, batch_tup, batch_idx):
-        input_t, mask_t, label_t, study_id_list = batch_tup
+        inputs, masks, labels, study_id_list = batch_tup
 
-        input_g = input_t.to(self.device, non_blocking=True)
-        mask_g = mask_t.to(self.device, non_blocking=True)
-        label_g = label_t.to(self.device, non_blocking=True)
-
-        logits_g, mask_pred_g = self(input_g, return_mask=True)
+        logits, mask_preds = self(inputs, return_mask=True)
+        probabilities = torch.nn.functional.softmax(logits, dim=-1).detach().cpu()
 
         cls_loss_g = self.cls_loss_func(
-            logits_g,
-            label_g,
+            logits,
+            labels,
         )
 
         seg_loss_g = self.seg_loss_func(
-            mask_pred_g,
-            mask_g,
+            mask_preds,
+            masks,
         )
 
         mean_loss = cls_loss_g.mean() + self.config.loss.params.seg_multiplier * seg_loss_g.mean()
 
+        average_precision = self.average_precision(probabilities, labels)
+
         self.log('train_loss', mean_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        self.log(
+            'train_ap', average_precision, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
 
         return mean_loss
 
     def validation_step(self, batch_tup, batch_idx):
-        input_t, mask_t, label_t, study_id_list = batch_tup
+        inputs, masks, labels, study_id_list = batch_tup
 
-        input_g = input_t.to(self.device, non_blocking=True)
-        mask_g = mask_t.to(self.device, non_blocking=True)
-        label_g = label_t.to(self.device, non_blocking=True)
-
-        logits_g, mask_pred_g = self(input_g, return_mask=True)
+        logits, mask_preds = self(inputs, return_mask=True)
+        probabilities = torch.nn.functional.softmax(logits, dim=-1).detach().cpu()
 
         cls_loss_g = self.cls_loss_func(
-            logits_g,
-            label_g,
+            logits,
+            labels,
         )
 
         seg_loss_g = self.seg_loss_func(
-            mask_pred_g,
-            mask_g,
+            mask_preds,
+            masks,
         )
 
         mean_loss = cls_loss_g.mean() + self.config.loss.params.seg_multiplier * seg_loss_g.mean()
 
+        average_precision = self.average_precision(probabilities, labels)
+
         self.log('val_loss', mean_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        self.log(
+            'val_ap', average_precision, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
 
         return mean_loss
 
