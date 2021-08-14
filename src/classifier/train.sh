e64938dd5b5517f1c7848acf423c9bfc146d0851
Yousef Rabi <yousofrabi@gmail.com>
2021-08-14 13:01:44 +1000

    Retrain 83 no scheduler

--- src/classifier/train.sh
+++ src/classifier/train.sh
@@ -1,10 +1,9 @@
 set -eux pipefail
 
-export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH='/home/yousef/deep-learning/kaggle/covid/src/'
 
-# python src/classifier/train.py --config src/classifier/config/83/fold-0.yml
-# python src/classifier/train.py --config src/classifier/config/83/fold-1.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-0.yml
+python src/classifier/train.py --config src/classifier/config/83/fold-1.yml
 python src/classifier/train.py --config src/classifier/config/83/fold-2.yml
 python src/classifier/train.py --config src/classifier/config/83/fold-3.yml
 python src/classifier/train.py --config src/classifier/config/83/fold-4.yml
